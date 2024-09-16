bea6abab831b10b374bac8f86b422708e6d59e41
tigran <tyavroyan@gmail.com>
2024-09-16 21:29:51 +0400

    deleting all spaces from line (except in quotes)

--- srcs/remove_spaces.c
+++ srcs/remove_spaces.c
@@ -6,7 +6,7 @@
 /*   By: tigran <tigran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 18:01:23 by tigran            #+#    #+#             */
-/*   Updated: 2024/09/16 20:13:48 by tigran           ###   ########.fr       */
+/*   Updated: 2024/09/16 21:28:40 by tigran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void _update_quote_info (bool* open, char* opened_ch, t_node_ptr curr);
 static void _remove_head_spaces(t_list_ptr line);
-static void _remove_spaces_till_one(t_list_ptr line, t_node_ptr curr);
+static void _remove_spaces(t_list_ptr line, t_node_ptr* curr);
 
 
 void ft_remove_spaces(t_list_ptr line)
@@ -29,10 +29,13 @@ void ft_remove_spaces(t_list_ptr line)
 	curr = line->head;
 	while (curr)
 	{
-		_update_quote_info(&open, &opened_ch, curr);
 		if (!open && ft_isspace(*curr->val))
-			_remove_spaces_till_one(line, curr);
-		curr = curr->next;
+			_remove_spaces(line, &curr);
+		if (curr)
+		{
+			_update_quote_info(&open, &opened_ch, curr);
+			curr = curr->next;
+		}
 	}
 	print_lt(line);
 }
@@ -66,20 +69,15 @@ static void _remove_head_spaces(t_list_ptr line)
 	}
 }
 
-static void _remove_spaces_till_one(t_list_ptr line, t_node_ptr curr)
+static void _remove_spaces(t_list_ptr line, t_node_ptr* curr)
 {
 	t_node_ptr	tmp;
 
-	tmp = curr;
-	while (curr && curr->next)
+	tmp = *curr;
+	while (*curr && ft_isspace(*(*curr)->val))
 	{
-		if (ft_isspace(*curr->val) && ft_isspace(*(curr->next->val))) {
-			remove_node_lt(line, curr->next);
-			curr = tmp;
-		}
-		else
-			break;
+		tmp = (*curr)->next;
+		remove_node_lt(line, *curr);
+		*curr = tmp;
 	}
-	if (line->tail && ft_isspace(*(line->tail->val)))
-		pop_back_lt(line);
 }
