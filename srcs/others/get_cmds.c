9b9a320ca8f56d3d4574a2c02f3a5d1e078005c6
Henrik Aleksanyan <healeksa@c2r11s6.42yerevan.am>
2024-11-15 22:24:11 +0400

    style(get_cmds): norm fix

--- srcs/others/get_cmds.c
+++ srcs/others/get_cmds.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 18:51:38 by tigran            #+#    #+#             */
-/*   Updated: 2024/11/15 21:46:52 by healeksa         ###   ########.fr       */
+/*   Updated: 2024/11/15 22:20:52 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,26 +52,8 @@ static t_node_ptr	__till_pipe(t_node_ptr curr)
 	return (curr);
 }
 
-void	parse_arg(t_node_ptr curr, t_minishell_ptr minishell, t_node_ptr head,
-		int i)
+t_node_ptr	execlop(t_node_ptr	curr, t_minishell_ptr minishell, int i)
 {
-	curr = head->next;
-	while (curr && *curr->val == '-')
-	{
-		push_back_lt(minishell->commands->cmds[i]->options, curr->val);
-		curr = curr->next;
-	}
-}
-
-static t_node_ptr	get_cmds_attr(t_minishell_ptr minishell, t_node_ptr head,
-		int i)
-{
-	t_node_ptr	curr;
-
-	curr = NULL;
-	if (!head || !head->next)
-		return (NULL);
-	parse_arg(curr, minishell, head, i);
 	while (curr)
 	{
 		if (_equal(curr->val, "|"))
@@ -89,6 +71,23 @@ static t_node_ptr	get_cmds_attr(t_minishell_ptr minishell, t_node_ptr head,
 		}
 	}
 	return (NULL);
+}
+
+static t_node_ptr	get_cmds_attr(t_minishell_ptr minishell, t_node_ptr head,
+		int i)
+{
+	t_node_ptr	curr;
+
+	curr = NULL;
+	if (!head || !head->next)
+		return (NULL);
+	curr = head->next;
+	while (curr && *curr->val == '-')
+	{
+		push_back_lt(minishell->commands->cmds[i]->options, curr->val);
+		curr = curr->next;
+	}
+	return (execlop(curr, minishell, i));
 }
 
 void	get_cmds(t_minishell_ptr minishell)
