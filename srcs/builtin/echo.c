05b7c93ce4f13891fc2f13f58168d388c10b6ad1
tigran <tyavroyan@gmail.com>
2024-11-16 14:41:50 +0400

    fixed: echo

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tigran <tigran@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 18:23:51 by tigran            #+#    #+#             */
-/*   Updated: 2024/11/10 19:36:13 by healeksa         ###   ########.fr       */
+/*   Updated: 2024/11/16 14:40:33 by tigran           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,23 @@ static t_node_ptr	check_flag(t_command_ptr command, bool *flag)
 	tmp = command->options->head;
 	while (tmp)
 	{
-		i = -1;
-		while (tmp->val[++i])
+		i = 0;
+		if (tmp->val[i] == '-')
+			++i;
+		while (tmp->val[i])
 		{
-			if (i == 0 && tmp->val[i] == '-')
+			if (tmp->val[i] == 'n')
+			{
+				++i;
 				continue ;
-			else if (i != 0 && tmp->val[i] == 'n')
-				continue ;
+			}
 			else
 				break ;
 		}
 		if (tmp->val[i] != '\0')
 			return (tmp);
 		if (i > 1)
-			*flag = 1;
+			*flag = true;
 		tmp = tmp->next;
 	}
 	return (NULL);
@@ -58,11 +61,9 @@ void	ft_echo(t_command_ptr command)
 	bool		flag;
 
 	output = NULL;
-	flag = 0;
+	flag = false;
 	move_back_lt(&command->options, command->args);
 	s = check_flag(command, &flag);
-	if (!s)
-		return ;
 	output = from_head_to_matrix_lt(s);
 	print_out(output);
 	if (!flag)
